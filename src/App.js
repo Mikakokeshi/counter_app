ea321684ae4613cfac660039aaac85caca0f889b
Mikako Iijima <mikakokeshi00124@gmail.com>
2017-09-27 14:46:44 +0800

    Added readme

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Welcome name="Jamie"/>
+        <Welcome name="Mika"/>
         <Today day="Wednesday"/>
         <Counter />
       </div>
--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 class Counter extends Component {
   constructor(props){
     super(props);
